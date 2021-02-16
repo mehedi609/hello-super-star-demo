b303c8b0412864ca50716919824f8cd16517d254
mehedi609 <mehedi609@gmail.com>
2021-02-16 10:27:04 +0600

    renamed some files and change folder strucure

--- src/components/main-content/layout/Main-Content-Layout.jsx
+++ src/components/main-content/layout/Main-Content-Layout.jsx
@@ -3,11 +3,11 @@ import { Route } from 'react-router-dom';
 import RightSidebarFriendList from './Right-Sidebar-FriendList';
 import LeftSidebarMenu from './Left-Sidebar-Menu';
 import TimeLine from '../Time-Line';
-import TimeLineFriends from './TimeLineFriends';
-import TimeLinePhotos from './TimeLinePhotos';
-import TimeLineVideos from './TimeLineVideos';
-import TimeLineGroups from './TimeLineGroups';
-import TimeLineAbout from './TimeLineAbout';
+import TimeLineFriends from '../Time-Line-Friends';
+import TimeLinePhotos from '../Time-Line-Photos';
+import TimeLineVideos from '../Time-Line-Videos';
+import TimeLineGroups from '../Time-Line-Groups';
+import TimeLineAbout from '../Time-Line-About';
 
 const MainContentLayout = () => {
   return (
